bec7ee056adc4ef0750f199c25933da9addca834
edward616 <Haibing.Zhou@software.dell.com>
2015-06-10 16:26:58 +0800

    xx

--- resources/MonitorLabs/MonitorSetup.bat
+++ resources/MonitorLabs/MonitorSetup.bat
@@ -1,6 +1,6 @@
 @echo off
 set currentPath=%~dp0
-set "psStript=powershell -ExecutionPolicy ByPass -File %currentPath%GetRemoteDesktopInfo.ps1"
+set "psStript=powershell -ExecutionPolicy ByPass -File %currentPath%collectLabInfo.ps1"
 echo %psStript%
-schtasks /create /sc minute /mo 1 /tn "MonitorSetup" /tr "%psStript%"
+schtasks /create /sc minute /mo 5 /tn "MonitorSetup" /tr "%psStript%"
 pause
